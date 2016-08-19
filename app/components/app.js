e75a462d99d94c44b69cf476d495e42acc8e20cb
Andrew Schreiber <schreiaj@gmail.com>
2016-08-19 14:14:24 -0400

    Now with a loop!

--- app/components/app.js
+++ app/components/app.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
+		const myArray = [0, 1, 2, 3, 4, 5, 6];
     return (
       <div>
-        Hello World
+        {myArray.map((el,i) => <div key={i}>{el}</div>)}
       </div>
     );
   }
